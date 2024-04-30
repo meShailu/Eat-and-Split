9b786dbd135364004ed896c71d346c43ddfaaa38
meShailu <123243572+meShailu@users.noreply.github.com>
2024-04-30 11:18:27 +0200

    Displaying the new friend form

--- Eat and Split/src/App.jsx
+++ Eat and Split/src/App.jsx
@@ -21,13 +21,26 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>Add Friend</Button>
       </div>
       <FormSplitBill />
     </div>
@@ -70,10 +83,6 @@ function Friend({ friend }) {
       <Button>Select</Button>
     </li>
   );
-}
-
-function Button({ children }) {
-  return <button className="button">{children}</button>;
 }
 
 function FormAddFriend() {
